c912289aedd6ccdf177812843c8be7f78f2959d3
Foraminiferatrv <ione.trv@gmail.com>
2024-10-20 20:31:52 +0300

    feat: Error message added

---
+++ src/app/brews/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="flex size-full items-center justify-center">
+      <h3 className="font-bold text-md">There was an error.</h3>
+    </div>
+  );
+}
